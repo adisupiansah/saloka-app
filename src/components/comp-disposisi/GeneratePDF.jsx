900d48e2f848ffac05514edabb0d4efbbfc22779
adisupiansah <adisupiansah15@gmail.com>
2025-03-28 05:36:59 +0700

    Updated 28-3-2025

--- src/components/comp-disposisi/GeneratePDF.jsx
+++ src/components/comp-disposisi/GeneratePDF.jsx
@@ -45,7 +45,7 @@ const GenaratePDF = ({ id }) => {
       const textWidth = doc.getTextWidth("POLRI DAERAH KEPUALAUAN RIAU");
       const startx = 57 - (textWidth / 2); // Pusatkan (jika teks di-center)
       const starty = 26; // Posisi Y garis (sesuaikan dengan teks)
-      doc.setLineWidth(0.5);
+      doc.setLineWidth(0.3);
       doc.line(startx, starty, startx + textWidth, starty);
       
       // Klasifikasi
@@ -53,10 +53,10 @@ const GenaratePDF = ({ id }) => {
       doc.text(`KLASIFIKASI : ${contohData.klasifikasi} / RAHASIA`, 135, 35);
       doc.text(`DERAJAT    : ${contohData.derajat} / KILAT`, 135, 40);
       
-      // Garis pemisah
+      // Garis luar tabel
       doc.setDrawColor(0);
-      doc.setLineWidth(0.5);
-      doc.line(20, 45, 190, 45);
+      doc.setLineWidth(0.3);
+      doc.line(20, 45, 190, 45); //atas
       doc.line(20, 45, 20, 285); // Kiri
       doc.line(190, 45, 190, 285); // Kanan
       doc.line(20, 285, 190, 285); // Bawah
@@ -68,9 +68,9 @@ const GenaratePDF = ({ id }) => {
       
       // Nomor dan Tanggal
       doc.setFont("helvetica", "normal");
-      doc.setFontSize(10);
+      doc.setFontSize(11);
       doc.text(`No. Disposisi : ${data.no_disposisi}`, 22, 60);
-      doc.text(`Diterima tgl  : ${moment.utc(data.createdAt).tz("Asia/Jakarta").format("DD-MM-YYYY - HH:mm:ss")}`, 130, 60);
+      doc.text(`Diterima tgl  : ${moment.utc(data.createdAt).tz("Asia/Jakarta").format("DD-MM-YYYY - HH:mm:ss")}`, 126, 60);
       
       // Tabel Utama
       const startY = 70;
@@ -82,11 +82,11 @@ const GenaratePDF = ({ id }) => {
       
       // garis atas dan bawah
       doc.setDrawColor(0);
-      doc.setLineWidth(0.5);
+      doc.setLineWidth(0.3);
       doc.line(20, startY, 190, startY); // Garis atas 
       doc.line(20, startY + 10, 190, startY + 10); // Garis bawah
       doc.setFont("helvetica", "bold");
-      doc.text("CATATAN BAGLOG", 105, startY + 7, { align: "center" });
+      doc.text("CATATAN BAGLOG", 105, startY + 6, { align: "center" });
      
       
       // Isi Tabel
@@ -95,29 +95,29 @@ const GenaratePDF = ({ id }) => {
       let currentY = startY + 15;
       
       // Baris 1
-      doc.text("Kapada Yth    :", 22, currentY);
-      doc.text(contohData.tujuan, 50, currentY);
+      doc.text("Kapada Yth       :", 22, currentY); // 15 karakter
+      doc.text(contohData.tujuan, 53, currentY);
       currentY += lineHeight;
       
       // Baris 2
-      doc.text("Surat dari    :", 22, currentY);
-      doc.text(data.satfung, 50, currentY);
+      doc.text("Surat dari          :", 22, currentY); // 15 karakter
+      doc.text(data.satfung, 53, currentY);
       currentY += lineHeight;
       
       // Baris 3
-      doc.text("Nomor surat    :", 22, currentY);
-      doc.text(data.no_surat, 50, currentY);
+      doc.text("Nomor surat      :", 22, currentY); // 15 karakter
+      doc.text(data.no_surat, 53, currentY);
       currentY += lineHeight;
       
       // Baris 4
-      doc.text("Tanggal surat    :", 22, currentY);
-      doc.text(data.tgl_surat, 50, currentY);
+      doc.text("Tanggal surat    :", 22, currentY); // 15 karakter
+      doc.text(data.tgl_surat, 53, currentY);
       currentY += lineHeight;
       
       // Baris 5 (Perihal multi-line)
-      doc.text("Perihal    :", 22, currentY);
+      doc.text("Perihal               :", 22, currentY); // 15 karakter
       const perihalLines = doc.splitTextToSize(data.perihal, 120);
-      doc.text(perihalLines, 50, currentY);
+      doc.text(perihalLines, 53, currentY);
       currentY += (lineHeight * perihalLines.length);
       
       // Tabel bagian bawah
@@ -125,29 +125,22 @@ const GenaratePDF = ({ id }) => {
       const colWidth = 85;
       const centerPage = 105; // Tengah halaman A4 (210mm/2)
       
-      // Header bagian bawah
       // 1. Teks "PELAKSANA TUGAS" di atas (rata tengah)
       doc.setFont("helvetica", "bold");
-      doc.text("PELAKSANA TUGAS", centerPage, bottomTableY - 5, { align: "center" });
+     
   
       // 2. Background abu-abu untuk header kolom
       // diganti dengan garis atas dan bawah
       doc.setDrawColor(0);
-      doc.setLineWidth(0.5);
+      doc.setLineWidth(0.3);
       doc.line(20, bottomTableY, 190, bottomTableY); // Garis atas
       doc.line(20, bottomTableY + 10, 190, bottomTableY + 10); // Garis bawah
-  
-      // garis tengah kolom
-      doc.setDrawColor(0);
-      doc.setLineWidth(0.5);
-      doc.line(105, bottomTableY, 105, bottomTableY + 50);
-     
-  
+      doc.line(centerPage, bottomTableY + 10, centerPage, bottomTableY + 50); // Garis tengah
+    
       // 3. Teks header kolom (dengan jarak yang lebih lebar)
-      doc.setFontSize(10); // Ukuran bisa disesuaikan
-      doc.text("KASUBAGPAL", 25, bottomTableY + 7); // Rata kiri dalam kotak
-      doc.text("KASUBAGFASKON", 170, bottomTableY + 7, { align: "right" }); // Rata kanan dalam kotak
-  
+      doc.setFontSize(11); // Ukuran bisa disesuaikan
+      doc.text("PELAKSANA TUGAS", centerPage, bottomTableY + 6, { align: "center" });
+     
       // Isi bagian bawah
       doc.setFont("helvetica", "normal");
       const bottomContent = [
@@ -166,21 +159,25 @@ const GenaratePDF = ({ id }) => {
       ];
       
       let bottomY = bottomTableY + 15;
+
+      // Loop untuk kolom kiri (bottomContent)
       bottomContent.forEach((row) => {
-        doc.text(row[0], 25, bottomY);
+        doc.text(row[0], 25, bottomY); // Kolom kiri dimulai dari x=25
         bottomY += lineHeight;
       });
-  
-      let bottomCon2Y = 110;
-      let btc = bottomCon2Y + 42
+      
+      // Reset bottomY untuk kolom kanan (bottomContent2)
+      bottomY = bottomTableY + 15;
+      
+      // Loop untuk kolom kanan (bottomContent2)
       bottomContent2.forEach((row) => {
-        doc.text(row[0], 138, btc);
-        btc += lineHeight;
+        doc.text(row[0], 125, bottomY); // Kolom kanan dimulai dari x=125
+        bottomY += lineHeight;
       });
   
       // garis pemisah antar kolong pelaksana tugas dan footer
       doc.setDrawColor(0);
-      doc.setLineWidth(0.5);
+      doc.setLineWidth(0.3);
       doc.line(20, bottomTableY + 50, 190, bottomTableY + 50);
       
       // Footer
@@ -190,12 +187,12 @@ const GenaratePDF = ({ id }) => {
   
       // garis tengah tanggal dan paraf
       doc.setDrawColor(0);
-      doc.setLineWidth(0.5);
+      doc.setLineWidth(0.3);
       doc.line(105, bottomY, 105, bottomY + 20);
   
       // garis pemisah
       doc.setDrawColor(0);
-      doc.setLineWidth(0.5);
+      doc.setLineWidth(0.3);
       doc.line(20, bottomY + 20, 190, bottomY + 20);
   
   
@@ -212,7 +209,7 @@ const GenaratePDF = ({ id }) => {
 
   return (
    <button className="btn-addtoprint" onClick={generatePDF}>
-         <FaPrint/>
+     <FaPrint/>
     </button>
   );
 };
